29813f2eb4fe8ba1093e25d46e2eb6dc1cf83d2f
yannCHANTREL <74556830+yannCHANTREL@users.noreply.github.com>
2021-11-09 14:23:25 +0100

    update map

--- Assets/Balle.cs
+++ Assets/Balle.cs
@@ -12,8 +12,14 @@ public class Balle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentSpeed = Vector3.zero;
+        currentSpeed = new Vector3(0f,0f,-20f);
+        transform.position += currentSpeed * Time.deltaTime;
         cubeList = GameObject.FindGameObjectsWithTag("Cube");
+        GameObject[] pinList = GameObject.FindGameObjectsWithTag("Pin");
+        foreach (var pin in pinList)
+        {
+            cubeList[cubeList.Length] = pin;
+        }
     }
 
     // Update is called once per frame
@@ -35,27 +41,38 @@ public class Balle : MonoBehaviour
     
     int DetectCollision(Vector3 currentPos, GameObject cube)
     {
-        // calcul scalar product
-        Vector3 cubePos = cube.transform.position;
-        Vector3 diffPos = new Vector3(currentPos.x - cubePos.x, currentPos.y - cubePos.y, currentPos.z - cubePos.z);
-        float scalarProductX = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.right));
-        float scalarProductY = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.up));
-        float scalarProductZ = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.forward));
-        
-        // verification collision
+        // initialisation value
         float rayonBalle = transform.localScale.x / 2;
         Vector3 cubeLocalScale = cube.transform.localScale;
         float distanceBeforeTouchX = rayonBalle + cubeLocalScale.x / 2;
         float distanceBeforeTouchY = rayonBalle + cubeLocalScale.y / 2;
         float distanceBeforeTouchZ = rayonBalle + cubeLocalScale.z / 2;
-        // collision ?
-        if (scalarProductY < distanceBeforeTouchY && scalarProductX < distanceBeforeTouchX && scalarProductZ < distanceBeforeTouchZ &&
-            scalarProductY > -distanceBeforeTouchY && scalarProductX > -distanceBeforeTouchX && scalarProductZ > -distanceBeforeTouchZ)
+        Vector3 cubePos = cube.transform.position;
+        
+        // verification collision
+        Vector3 futurPos = currentPos;
+        futurPos += currentSpeed * Time.deltaTime;
+        Vector3 diffPosFutur = new Vector3(currentPos.x - futurPos.x, currentPos.y - futurPos.y, currentPos.z - futurPos.z) / 100;
+        for (int i = 0; i < 100; i++)
         {
-            // on what surface ?
-            if (scalarProductX < distanceBeforeTouchX && scalarProductX > distanceBeforeTouchX - 0.1f || scalarProductX > -distanceBeforeTouchX && scalarProductX < -distanceBeforeTouchX + 0.1f) return 1;
-            if (scalarProductY < distanceBeforeTouchY && scalarProductY > distanceBeforeTouchY - 0.1f || scalarProductY > -distanceBeforeTouchY && scalarProductY < -distanceBeforeTouchY + 0.1f) return 2;
-            if (scalarProductZ < distanceBeforeTouchZ && scalarProductZ > distanceBeforeTouchZ - 0.1f || scalarProductZ > -distanceBeforeTouchZ && scalarProductZ < -distanceBeforeTouchZ + 0.1f) return 3;
+            // simulate move
+            futurPos += diffPosFutur;
+            
+            // calcul scalar product
+            Vector3 diffPos = new Vector3(futurPos.x - cubePos.x, futurPos.y - cubePos.y, futurPos.z - cubePos.z);
+            float scalarProductX = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.right));
+            float scalarProductY = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.up));
+            float scalarProductZ = Vector3.Dot(diffPos, Vector3.Normalize(cube.transform.forward));
+
+            // collision ?
+            if (scalarProductY < distanceBeforeTouchY && scalarProductX < distanceBeforeTouchX && scalarProductZ < distanceBeforeTouchZ &&
+                scalarProductY > -distanceBeforeTouchY && scalarProductX > -distanceBeforeTouchX && scalarProductZ > -distanceBeforeTouchZ)
+            {
+                // on what surface ?
+                if (scalarProductX < distanceBeforeTouchX && scalarProductX > distanceBeforeTouchX - 0.1f || scalarProductX > -distanceBeforeTouchX && scalarProductX < -distanceBeforeTouchX + 0.1f) return 1;
+                if (scalarProductY < distanceBeforeTouchY && scalarProductY > distanceBeforeTouchY - 0.1f || scalarProductY > -distanceBeforeTouchY && scalarProductY < -distanceBeforeTouchY + 0.1f) return 2;
+                if (scalarProductZ < distanceBeforeTouchZ && scalarProductZ > distanceBeforeTouchZ - 0.1f || scalarProductZ > -distanceBeforeTouchZ && scalarProductZ < -distanceBeforeTouchZ + 0.1f) return 3;
+            }
         }
         return 0;
     }
